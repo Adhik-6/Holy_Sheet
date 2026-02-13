4ee2deb6bfa328169b7d184e663fdc627c6f1688
Adhik-6 <adhik.m10a@gmail.com>
2026-02-13 23:03:37 +0530

    fix: CORS

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,6 +5,19 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          // Notice: No ports! Just the raw Capacitor origins
+          { key: "Access-Control-Allow-Origin", value: "http://localhost, capacitor://localhost" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
